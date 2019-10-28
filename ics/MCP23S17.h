70cc29f6246d0df5a38d8eeb6707104b3621567c
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-10-27 17:29:53 -0700

    Define read and write to start

--- ics/MCP23S17.h
+++ ics/MCP23S17.h
@@ -30,18 +30,67 @@
 namespace bonuspin 
 {
 
-template<int chipEnable, byte address>
+template<int chipEnable, byte address, int reset = -1>
 class MCP23S17 {
     static_assert((address & 0b111) == address, "Provided address is too large!");
+    private:
+        static constexpr auto generateByte(bool a, bool b, bool c, bool d, bool e, bool f, bool g, bool h) noexcept {
+            byte output = 0;
+            output |= (b ? 0b0000'0001 : 0);
+            output |= (b ? 0b0000'0010 : 0);
+            output |= (c ? 0b0000'0100 : 0);
+            output |= (d ? 0b0000'1000 : 0);
+            output |= (e ? 0b0001'0000 : 0);
+            output |= (f ? 0b0010'0000 : 0);
+            output |= (g ? 0b0100'0000 : 0);
+            output |= (h ? 0b1000'0000 : 0);
+            return output;
+        }
+        static constexpr auto generateIOConByte(bool intPolarity, bool odr, bool haen, bool disslw, bool seqop, bool mirror, bool bank) noexcept {
+            return generateByte(false, intPolarity, odr, haen, disslw, seqop, mirror, bank);
+        }
     public:
         using Self = MCP23S17<chipenable, address>;
         using CSEnabler = HoldPinLow<chipEnable>;
         static constexpr auto ChipEnablePin = chipEnable;
         static constexpr auto BusAddress = address;
-        constexpr auto getChipEnablePin() const noexcept { return chipEnable; }
-        constexpr auto getSPIAddress() const noexcept { return address; }
+        static constexpr auto ResetPin = reset;
+        static constexpr auto HasResetPin = (ResetPin >= 0);
+        constexpr auto getChipEnablePin() const noexcept { return ChipEnablePin; }
+        constexpr auto getSPIAddress() const noexcept { return BusAddress; }
+        constexpr auto getResetPin() const noexcept { return ResetPin; }
+        constexpr auto hasResetPin() const noexcept { return ResetPin >= 0; }
     public:
-        //MCP23S17()
+        MCP23S17() {
+            pinMode(ChipEnablePin, OUTPUT);
+            digitalWrite(ChipEnablePin, HIGH);
+            // we could do if constexpr here T_T if c++17 is default active
+            if (hasResetPin()) {
+                
+            }
+        }
+        class ReadOperation final { };
+        class WriteOperation final { };
+        constexpr byte generateOpcode(ReadOperation) const noexcept {
+            return 0b0100'0000 | (getSPIAddress() << 1) | 1;
+        }
+        constexpr byte generateOpcode(WriteOperation) const noexcept {
+            return 0b0100'0000 | (getSPIAddress() << 1);
+        }
+
+        byte read(byte registerAddress) noexcept {
+            CSEnabler enable;
+            SPI.transfer(static_cast<uint8_t>(generateOpcode(ReadOperation{})));
+            SPI.transfer(static_cast<uint8_t>(registerAddress));
+            return SPI.transfer(0x00);
+        }
+        void write(byte registerAddress, byte value) noexcept {
+            CSEnabler enable;
+            SPI.transfer(static_cast<uint8_t>(generateOpcode(WriteOperation{})));
+            SPI.transfer(static_cast<uint8_t>(registerAddress));
+            SPI.transfer(static_cast<uint8_t>(value));
+        }
+    private:
 };
 
 
